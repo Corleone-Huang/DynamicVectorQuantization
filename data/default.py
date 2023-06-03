b13973e15a6818861730ca529c2b446c59f11a03
huangmq <huangmq@mail.ustc.edu.ccn>
2023-06-03 03:07:10 +0000

    .

--- data/default.py
+++ data/default.py
@@ -2,17 +2,8 @@ from easydict import EasyDict as edict
 
 DefaultDataPath = edict()
 
-DefaultDataPath.FFHQ = edict()
-DefaultDataPath.FFHQ.train_lmdb = "/home/huangmq/AdaptiveVectorQuantization/data/FFHQ/FFHQ_lmdb/train"
-DefaultDataPath.FFHQ.val_lmdb = "/home/huangmq/AdaptiveVectorQuantization/data/FFHQ/FFHQ_lmdb/val"
-
-DefaultDataPath.FFHQ.root = "/home/huangmq/Datasets/FFHQ"
-
-DefaultDataPath.CelebAHQ = edict()
-DefaultDataPath.CelebAHQ.root = "/home/huangmq/Datasets/CelebA/CelebAHQ/CelebA-HQ"
-
 DefaultDataPath.ImageNet = edict()
 
-DefaultDataPath.ImageNet.root = "/home/huangmq/Datasets/ImageNet"
-DefaultDataPath.ImageNet.train_write_root = "/home/huangmq/Datasets/ImageNet/train"
-DefaultDataPath.ImageNet.val_write_root = "/home/huangmq/Datasets/ImageNet/val"
+DefaultDataPath.ImageNet.root = "Your Data Path/Datasets/ImageNet"
+DefaultDataPath.ImageNet.train_write_root = "Your Data Path/Datasets/ImageNet/train"
+DefaultDataPath.ImageNet.val_write_root = "Your Data Path/Datasets/ImageNet/val"
